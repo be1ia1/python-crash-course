ee629597814679936214b4c7eeb449463f561b16
Andriy Gula <gula@pr.km.ua>
2021-05-17 08:13:11 +0300

    some home work

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,7 +7,11 @@ class Settings:
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.flee_direction = 1
+
--- tasks/rocket.py
+++ tasks/rocket.py
@@ -7,28 +7,79 @@ class Rocket:
         self.screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
         self.bg_color = (230, 230, 230)
         pygame.display.set_caption("Rocket Game")
+        self.rocket = Ship(self)
 
     def _update_screen(self):
         self.screen.fill(self.bg_color)
+        self.rocket.blitme()
         pygame.display.flip()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_UP:
+            self.rocket.mov_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.mov_down = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket.mov_left = True
+        elif event.key == pygame.K_RIGHT:
+            self.rocket.mov_right = True
 
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_UP:
+            self.rocket.mov_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.mov_down = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket.mov_left = False
+        elif event.key == pygame.K_RIGHT:
+            self.rocket.mov_right = False
 
     def run_game(self):
         while True:
             self._check_events()
+            self.rocket.update()
             self._update_screen()
 
+class Ship:
+    def __init__(self, rk_game):
+        self.screen = rk_game.screen
+        self.screen_rect = rk_game.screen.get_rect()
+        self.image = pygame.image.load('Daco_5228169-1.bmp')
+        self.rect = self.image.get_rect()
+        self.rect.center = self.screen_rect.center
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        self.mov_right = False
+        self.mov_left = False
+        self.mov_up = False
+        self.mov_down = False
+
+    def update(self):
+        if self.mov_up and self.rect.top > self.screen_rect.top:
+            self.y -= 0.3
+        elif self.mov_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += 0.3
+        elif self.mov_right and self.rect.right < 800:
+            self.x += 0.3
+        elif self.mov_left and self.rect.left > 0:
+            self.x -= 0.3
+        self.rect.y = self.y
+        self.rect.x = self.x
+        # pass
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
 
 if __name__ == '__main__':
     rk = Rocket()
